107ea1c92fc34d3f27c3b7a1c66f9b042d37604a
Armik Chopda <armikchopda35280@gmail.com>
2026-04-21 09:47:38 +0530

    exception

--- Python_Basic/exception.py
+++ Python_Basic/exception.py
@@ -7,7 +7,55 @@ else:
     print("error not found")         
 finally:
     print("armik")
+
+try:
+    import armik
+except ImportError as e:
+    print(e)
+
+
     
+try:
+    d={"id":1,"name":"armik","email":"armik@gmail.com"}
+    print(d['idw'])
+except KeyError as e:
+    print(e)
+
+
+try:
+    "armik".test()
+except AttributeError as a:
+    print(a)   
+
+try:
+    l=[1,2,3,4,4]
+    print(l[8])
+except IndexError as e:
+    print(e)            
+
+
+try:
+    12+"qww"
+except TypeError as e:
+    print(e)   
+
+
+try:
+    with open("armik.txt",'r') as f:
+        f.read()
+except FileNotFoundError as e:
+    print(e)  
+
+print(dir(Exception) )   
+
+try:
+    l=[1,2]
+    print(l[3])
+   
+except IndexError as e:
+    print("armik",e)   
+except Exception as e:
+    print(e)    
 
 
 
